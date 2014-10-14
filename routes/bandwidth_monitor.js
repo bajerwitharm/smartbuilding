031094a093b55aafb874b5db5eb81c0bb434174c
plmabaj <marcin.bajer@pl.abb.com>
2014-10-14 17:01:41 +0200

    Fixes to include stats from br-lan in sql queries

--- routes/bandwidth_monitor.js
+++ routes/bandwidth_monitor.js
@@ -1,13 +1,16 @@
 var database;
 var Client = require('node-rest-client').Client;
 var restClient = new Client();
+var router_ip = "192.168.1.1";
+var path_to_usage = "/cgi-bin/usage"
+var check_frequency = 900000;	// 15 minutes
     
 module.exports.initBandwidthMonitor = function(db) {
 	database=db;
 	setInterval(function() {
-		restClient.get('http://192.168.1.1/cgi-bin/usage', function(data) {
+		restClient.get('http://'+router_ip+path_to_usage, function(data) {
 			database.insertNewUsage(data);
 		})
-    }, 900000);
+    }, check_frequency);
 }
 
