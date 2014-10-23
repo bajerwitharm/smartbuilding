83648e474f939109cb7a38951b0fb9689fd9f8d3
plmabaj <marcin.bajer@pl.abb.com>
2014-10-24 00:13:53 +0200

    try/catch for log parsing

--- routes/logger.js
+++ routes/logger.js
@@ -1,51 +1,52 @@
 var database;
 
 var parseLogEntry = function(logentry) {
-    try {
-	switch(logentry.program) {
-	case "dnsmasq":
-		database.insertNewQuery(logentry);
-		break;
-	case "dnsmasq-dhcp":
-		database.insertNewUser(logentry);
-		break;
-	case "radiusd":
-		database.insertNewConnection(logentry);
-		break;	
-	default:
-		database.insertNewLogEntry(logentry);
-		break;
-	}
-    }
-    catch(err) {
 
+    switch (logentry.program) {
+    case "dnsmasq":
+	database.insertNewQuery(logentry);
+	break;
+    case "dnsmasq-dhcp":
+	database.insertNewUser(logentry);
+	break;
+    case "radiusd":
+	database.insertNewConnection(logentry);
+	break;
+    default:
+	database.insertNewLogEntry(logentry);
+	break;
     }
+
 }
-    
+
 module.exports.initLogger = function(db) {
-	database=db;
-	var datagram;
-	var server = require('net').createServer(function (stream) {
-        stream.setEncoding('utf8');
- //       stream.addListener("data", function (slice) {
-        	//datagram=datagram+slice;
-        //});
-        stream.addListener("data", function (slice) {
-        	datagram = datagram + slice;
-        	if (slice.length<3472){
-        		var logentries = JSON.parse("["+slice.slice(0,-1)+"]");
-                for (var logentry in logentries) {
-                  logentry = logentries[logentry];
-                  if (logentry.length == 0) {
-                    continue;
-                  }
-                  parseLogEntry(logentry);
-                 	        		
-                };
-                datagram = "";
-        	}        	
-        });
-      }); 
+    database = db;
+    var datagram;
+    var server = require('net').createServer(
+	    function(stream) {
+		stream.setEncoding('utf8');
+		// stream.addListener("data", function (slice) {
+		// datagram=datagram+slice;
+		// });
+		stream.addListener("data", function(slice) {
+		    datagram = datagram + slice;
+		    if (slice.length < 3472) {
+			try {
+			    var logentries = JSON.parse("["
+				    + slice.slice(0, -1) + "]");
+			    for ( var logentry in logentries) {
+				logentry = logentries[logentry];
+				if (logentry.length == 0) {
+				    continue;
+				}
+				parseLogEntry(logentry);
+			    }
+			} catch (err) {
+			}
+
+			datagram = "";
+		    }
+		});
+	    });
     server.listen(514, '0.0.0.0');
 }
-
