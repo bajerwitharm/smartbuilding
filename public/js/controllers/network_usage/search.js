bd858ec907f08ef5b45ee8f418ab9049e698efda
plmabaj <marcin.bajer@pl.abb.com>
2014-11-25 20:55:20 +0100

    Added latex publication to repo

--- public/js/controllers/network_usage/search.js
+++ public/js/controllers/network_usage/search.js
@@ -1,4 +1,11 @@
 salwatorskaControllers.controller('networkSearchController', [ '$scope',
 	'$rootScope', '$filter', 'orderByFilter', 'databaseProvider',
 	function($scope, $rootScope, $filter, orderByFilter, databaseProvider) {
+    		$scope.setAllView = function(){
+    		    $scope.selectedView = 'all';
+    		}
+    		$scope.setMonthsView = function(){
+    		    $scope.selectedView = 'months';
+    		}
+    		$scope.selectedView = 'all';
 	} ]);
