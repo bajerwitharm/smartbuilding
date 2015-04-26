0dafaabc0b762893ccc8b0a56b6f5ab19eb354f3
plmabaj <marcin.bajer@pl.abb.com>
2015-04-26 18:39:30 +0200

    Parser added to relay_board.js

--- WWW/routes/relay_board.js
+++ WWW/routes/relay_board.js
@@ -1,365 +1,82 @@
-var _ = require('struct-fu');
-var com = require("serialport");
-var serialPort = new com.SerialPort("/dev/ttyUSB0", {
-	baudRate : 4800,
-	dataBits : 8,
-	parity : 'none',
-	stopBits : 1,
-	flowControl : false,
-	parser : com.parsers.relayboard()
+(function (exports, require, module, __filename, __dirname) { 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var KameraWejscie = require('./routes/foscam.js');
+var KameraParter = require('./routes/acti.js');
+var sqliteDbContext = require('./data/database_mysql.js');//change to database_sqlite.js if needed
+var database = require('./routes/database.js');
+var logParser = require('./routes/logger.js');
+var onvifCameras = require('./routes/onvif.js'); 
+var bandwidthMonitor = require('./routes/bandwidth_monitor.js');
+var statsMonitor = require('./routes/stats_monitor.js');
+var relayBoard = require('./routes/relay_board.js');
+
+var app = express();
+
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('title','www.salwatorska.pl');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, '/public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+app.get('/users', user.list);
+app.get('/recordKameraWejscie', KameraWejscie.recordCamera);
+app.get('/recordKameraParter', KameraParter.recordCamera);
+app.get('/getLiveKameraWejscie', KameraWejscie.getLiveCamera);
+app.get('/getLiveKameraParter', KameraParter.getLiveCamera);
+app.get('/getLiveKameraPietro1', onvifCameras.getLiveCamera);
+app.get('/getLiveKameraPietro2', onvifCameras.getLiveCamera2);
+app.get('/getUsersInfo', database.getUsersInfo);
+app.get('/getConnectionsByHour', database.getConnectionsByHour);
+app.get('/getConnectionsByWeekday', database.getConnectionsByWeekday);
+app.get('/getConnectionsByMonthday', database.getConnectionsByMonthday);
+app.get('/getConnectionsByMonth', database.getConnectionsByMonth);
+app.get('/getConnectionsByAP', database.getConnectionsByAP);
+app.get('/getConnectionsInTime', database.getConnectionsInTime);
+app.get('/getConnectionsLast', database.getConnectionsLast);
+app.get('/getUrlsLast', database.getUrlsLast);
+app.get('/getOtherLogsLast', database.getOtherLogsLast);
+app.get('/getUsageByHour', database.getUsageByHour);
+app.get('/getUsageByWeekday', database.getUsageByWeekday);
+app.get('/getUsageByMonthday', database.getUsageByMonthday);
+app.get('/getUsageByMonth', database.getUsageByMonth);
+app.get('/getUsageByAP', database.getUsageByAP);
+app.get('/getRelayBoardInfo', relayBoard.requestInfo);
+app.get('/writeTelegram', relayBoard.writeTelegram);
+
+http.createServer(app).listen(app.get('port'), '0.0.0.0', function(){
+    // Find out which user used sudo through the environment variable
+    var uid = parseInt(process.env.SUDO_UID);
+    // Set our server's uid to that user
+    if (uid) process.setuid(uid);
+    console.log('Server\'s UID is now ' + process.getuid());
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-var busMaster = 0x0A
-var relayBoard = 0x0B
-var startChar = 0x7E
-var action_triggered_e = 0xDE
-var trigger_action_e = 0xDF
-var get_info_e = 0xEA 	
-
-//
-// var converter = _.struct([
-// _.struct('header', [
-// _.uint8('start'),
-// _.uint8('source'),
-// _.uint8('destination'),
-// _.uint8('fc'),
-// _.uint8('size') ]),
-// _.struct('actuator', [
-// _.uint8('output_on'),
-// _.uint8('output_off'),
-// _.uint8('output_toggle'),
-// _.uint8('state_on'),
-// _.uint8('state_off'),
-// _.uint8('state_toggle')]),
-// ]);
-
-var inputs = [ 'KitchenSw1', "KitchenSw2", "RoomSw1", "RoomSw2", "CorridorSw",
-		"ToiletOutSw", "ToiletInSw", "KitchenMotion"].reverse().concat(["RoomMotion",
-		"CorridorMotion", "", "", "", "", "", "" ].reverse());
-var outputs = [ "Kitchen1", "Kitchen2", "Room1", "Room2", "Corridor",
-		"ToiletIn", "ToiletOut", "" ].reverse();
-var states = [ "KitchenSw1", "KitchenSw2", "KitchenMotion", "RoomSw1",
-		"RoomSw2", "RoomMotion", "CorridorSw", "CorritorMotion"].reverse().concat(["","HeardBeat", "", "", "", "","","" ].reverse());
-
-var genereteBitStruct = function(elements) {
-	var result = [];
-	elements.forEach(function(element) {
-		result.push(_.bool(element));
-	});
-	return result;
-}
-
-var header_t = _.struct('header', [ _.uint8('start'), _.uint8('source'),
-		_.uint8('destination'), _.uint8('fc'), _.uint8('size') ]);
-
-var actuator_t = _.struct('actuator', [
-		_.struct('output_on', genereteBitStruct(outputs)),
-		_.struct('output_off', genereteBitStruct(outputs)),
-		_.struct('output_toggle', genereteBitStruct(outputs)),
-		_.struct('state_on', genereteBitStruct(states)),
-		_.struct('state_off', genereteBitStruct(states)),
-		_.struct('state_toggle', genereteBitStruct(states)) ]);
-
-var activator_t = _.struct('activator', [
-		_.struct('input_on', genereteBitStruct(inputs)),
-		_.struct('input_off', genereteBitStruct(inputs)),
-		_.struct('output_on', genereteBitStruct(outputs)),
-		_.struct('output_off', genereteBitStruct(outputs)),
-		_.struct('state_on', genereteBitStruct(states)),
-		_.struct('state_off', genereteBitStruct(states)) ]);
-
-var timer_t = _.struct('timer', [ _.uint16('time_preload'),
-		_.uint16('time_current') ]);
-
-var info_t = _.struct('info', [ _.struct('inputs', genereteBitStruct(inputs)),
-		_.struct('outputs', genereteBitStruct(outputs)),
-		_.struct('states', genereteBitStruct(states)) ]);
-
-var crc_t = _.uint8('crc');
-
-var trigger_action_t = _.struct([ header_t, actuator_t, crc_t ]);
-
-// trigger_action
-// { header: { start: 126, source: 250, destination: 251, fc: 223, size: 6 },
-// actuator:
-// { output_on:
-// { Kitchen1: false,
-// Kitchen2: false,
-// Room1: false,
-// Room2: false,
-// Corridor: false,
-// ToiletIn: false,
-// ToiletOut: false,
-// NotUsed: false },
-// output_off:
-// { Kitchen1: false,
-// Kitchen2: false,
-// Room1: false,
-// Room2: false,
-// Corridor: false,
-// ToiletIn: false,
-// ToiletOut: false,
-// NotUsed: false },
-// output_toggle:
-// { Kitchen1: true,
-// Kitchen2: true,
-// Room1: true,
-// Room2: true,
-// Corridor: true,
-// ToiletIn: true,
-// ToiletOut: true,
-// NotUsed: true },
-// state_on:
-// { KitchenSw1: false,
-// KitchenSw2: false,
-// KitchenMotion: false,
-// RoomSw1: false,
-// RoomSw2: false,
-// RoomMotion: false,
-// CorridorSw: false,
-// CorritorMotion: false },
-// state_off:
-// { KitchenSw1: false,
-// KitchenSw2: false,
-// KitchenMotion: false,
-// RoomSw1: false,
-// RoomSw2: false,
-// RoomMotion: false,
-// CorridorSw: false,
-// CorritorMotion: false },
-// state_toggle:
-// { KitchenSw1: false,
-// KitchenSw2: false,
-// KitchenMotion: false,
-// RoomSw1: false,
-// RoomSw2: false,
-// RoomMotion: false,
-// CorridorSw: false,
-// CorritorMotion: false } },
-// crc: 171 }
-//
-// [ 0x7E, 0xFA, 0xFB, 0xDF, 0x06, 0x00, 0x00, 0xFF,
-// 0x00, 0x00, 0x00, 0xAB ]
-
-var action_triggered_t = _.struct([ header_t, activator_t, actuator_t, timer_t,
-		crc_t ]);
-
-// { header: { start: 126, source: 251, destination: 250, fc: 222, size: 18 },
-// activator:
-// { input_on:
-// { CorridorMotion: false,
-// RoomMotion: true,
-// KitchenMotion: false,
-// ToiletInSw: false,
-// ToiletOutSw: false,
-// CorridorSw: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenSw2: false,
-// KitchenSw1: false },
-// input_off:
-// { CorridorMotion: false,
-// RoomMotion: false,
-// KitchenMotion: false,
-// ToiletInSw: false,
-// ToiletOutSw: false,
-// CorridorSw: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenSw2: false,
-// KitchenSw1: false },
-// output_on:
-// { NotUsed: false,
-// ToiletOut: false,
-// ToiletIn: false,
-// Corridor: false,
-// Room2: false,
-// Room1: false,
-// Kitchen2: false,
-// Kitchen1: false },
-// output_off:
-// { NotUsed: false,
-// ToiletOut: false,
-// ToiletIn: false,
-// Corridor: false,
-// Room2: false,
-// Room1: false,
-// Kitchen2: false,
-// Kitchen1: false },
-// state_on:
-// { CorritorMotion: false,
-// CorridorSw: false,
-// RoomMotion: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenMotion: false,
-// KitchenSw2: false,
-// KitchenSw1: false },
-// state_off:
-// { CorritorMotion: false,
-// CorridorSw: false,
-// RoomMotion: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenMotion: false,
-// KitchenSw2: false,
-// KitchenSw1: false } },
-// actuator:
-// { output_on:
-// { NotUsed: false,
-// ToiletOut: false,
-// ToiletIn: false,
-// Corridor: false,
-// Room2: false,
-// Room1: false,
-// Kitchen2: false,
-// Kitchen1: false },
-// output_off:
-// { NotUsed: false,
-// ToiletOut: false,
-// ToiletIn: false,
-// Corridor: false,
-// Room2: false,
-// Room1: false,
-// Kitchen2: false,
-// Kitchen1: false },
-// output_toggle:
-// { NotUsed: false,
-// ToiletOut: false,
-// ToiletIn: false,
-// Corridor: false,
-// Room2: false,
-// Room1: false,
-// Kitchen2: false,
-// Kitchen1: true },
-// state_on:
-// { CorritorMotion: false,
-// CorridorSw: false,
-// RoomMotion: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenMotion: false,
-// KitchenSw2: false,
-// KitchenSw1: false },
-// state_off:
-// { CorritorMotion: false,
-// CorridorSw: false,
-// RoomMotion: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenMotion: false,
-// KitchenSw2: false,
-// KitchenSw1: false },
-// state_toggle:
-// { CorritorMotion: false,
-// CorridorSw: false,
-// RoomMotion: false,
-// RoomSw2: false,
-// RoomSw1: false,
-// KitchenMotion: false,
-// KitchenSw2: false,
-// KitchenSw1: true } },
-// timer: { time_preload: 256, time_current: 256 },
-// crc: 171 }
-// Buffer([ 0x7E, 0xFB, 0xFA, 0xDE, 0x12, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
-// 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x01, 0x01, 0x00, 0x01, 0x00, 0xAB
-// ])
-
-var get_info_t_req = _.struct([ header_t,
-// _.uint8('data'),
-crc_t ]);
-
-var get_info_t_res = _.struct([ header_t, info_t, crc_t ]);
-
-module.exports.requestInfo = function(req, res) {
-	telegram = {
-		header : {
-			start : startChar,
-			source : busMaster,
-			destination : relayBoard,
-			fc : get_info_e,
-			size : 0x00
-		},
-		crc : 0xAB
-	};
-	serialPort.write(get_info_t_req.pack(telegram));
-	console.log(get_info_t_req.pack(telegram));
-	serialPort.once('data', function(data) {
-		//console.log(data);
-		res.send(get_info_t_res.unpack(data));
-	});
-}
-
-var cleanJson = function(obj){
-	  var isArray = obj instanceof Array;
-	  for (var k in obj){
-	    if ((obj[k]==null)||(obj[k]==false)) {
-	    	if (isArray) {
-	    		if (obj[k].length > 0) {
-	    			obj.splice(k,1)	
-	    		}
-	    		else {
-	    			delete obj[k];
-	    		} 
-	    	} else {
-	    		delete obj[k];
-	    	}
-	    } else if (typeof obj[k]=="object") {
-	    	cleanJson(obj[k]);
-	    	if (Object.getOwnPropertyNames(obj[k]).length == 0) {
-	    		delete obj[k];
-	    	}
-	    }
-	  }
-	  
-}
-
-serialPort.on('data', function(data) {
-	console.log(data);
-	//try {
-		switch(data[3]) {
-		case get_info_e:
-			console.log(get_info_t_res.unpack(data));
-			break;
-		case action_triggered_e:
-			data = action_triggered_t.unpack(data);
-			cleanJson(data);
-			console.log(data);
-			break;	
-		} 
-
-	//} catch (err) {
-	//}
+sqliteDbContext.createDatabase();
+logParser.initLogger(sqliteDbContext);
+onvifCameras.initOnvif(sqliteDbContext);
+//bandwidthMonitor.initBandwidthMonitor(sqliteDbContext);
+//statsMonitor.initStatsMonitor(sqliteDbContext,"192.168.1.1");
 });
-
-module.exports.writeTelegram = function(req, res) {
-	telegram = {
-		header : {
-			start : startChar,
-			source : busMaster,
-			destination : relayBoard,
-			fc : trigger_action_e,
-			size : 9
-		},
-		actuator : {
-			output_toggle : {
-				Kitchen1 : true,
-				Kitchen2 : true,
-				Room1 : true,
-				Room2 : true,
-				Corridor : true,
-				ToiletIn : true,
-				ToiletOut : true,
-				NotUsed : true
-			}
-		},
-		crc : 171
-	}
-	serialPort.write(trigger_action_t.pack(telegram));
-	serialPort.once('data', function(data) {
-		data = trigger_action_t.unpack(data);
-		cleanJson(data);
-		res.send(data);
-	});
-
-}
