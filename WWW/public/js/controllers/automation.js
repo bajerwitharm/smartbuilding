befb791a805cbb850005c2908b33a5dd9dde6ef9
plmabaj <marcin.bajer@pl.abb.com>
2015-06-06 15:42:31 +0200

    Some changes before leaving Rajska

--- WWW/public/js/controllers/automation.js
+++ WWW/public/js/controllers/automation.js
@@ -1,39 +1,11 @@
-salwatorskaControllers.controller('automationController', [ '$scope',
-	'$rootScope', 'databaseProvider',
+salwatorskaControllers.controller('automationController', [
+	'$scope',
+	'$rootScope',
+	'databaseProvider',
 
 	function($scope, $rootScope, databaseProvider) {
-	    $scope.state = {
-		header : {
-		    start : 126,
-		    source : 251,
-		    destination : 250,
-		    fc : 222,
-		    size : 18
-		},
-		inputs : {
-			NotUsed : false,
-
-		},
-		outputs : {
-			NotUsed : false,
-			ToiletOut : false,
-			ToiletIn : false,
-			Corridor : false,
-			Room2 : false,
-			bulb_enterance : false,
-			bulb_stairsleft : false,
-			bulb_stairsright : true
-		},
-		states: {
-			CorritorMotion : false,
-			CorridorSw : false,
-			RoomMotion : false,
-			RoomSw2 : false,
-			RoomSw1 : false,
-			KitchenMotion : false,
-			KitchenSw2 : false,
-			KitchenSw1 : false  
-		},
-		crc : 171
-	    }
-	} ]);
+	    $http.get('/getRelayBoardInfo').success(
+		    function(data, status, headers, config) {
+			$scope.state = data
+		    });
+	} ])
