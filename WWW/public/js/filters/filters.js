88f7f24b21c1d2a37c08572434c8617e95a84fd4
root <root@SalwatorskaSmallSerwer.(none)>
2015-07-08 00:31:22 +0200

    postgres cd3

--- WWW/public/js/filters/filters.js
+++ WWW/public/js/filters/filters.js
@@ -54,7 +54,7 @@
 	        }
 	        var sum = 0;
 	        for (var i = 0; i < data.length; i++) {
-	            sum = sum + data[i][key];
+	            sum = sum + parseInt(data[i][key]);
 	        }
 	        return sum;
 	    };
